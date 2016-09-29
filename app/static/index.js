65105499af6e43e904a9c38da03dd1a5ff21704f
Michael Ferraco <michael.ferraco@capitalone.com>
2016-09-29 17:52:36 -0400

    Updating account selection logic

--- app/static/index.js
+++ app/static/index.js
@@ -41,8 +41,24 @@ $(function(){
 		selectingToAccount = false;
 	});
 
+	var resetSelections = {
+		'selectedToAccount': false,
+		'selectedFromAccount': false
+	};
+
+	// set all accounts as not selected
+	$(".account").data(resetSelections);
+
 	$(".account").click(function() {
+		var isToAccountSelected = $(this).data('selectedToAccount');
+		var isFromAccountSelected = $(this).data('selectedFromAccount')
+		var selected = isToAccountSelected || isFromAccountSelected;
+		
 		if (selectingToAccount) {
+			// mark this account as selected
+			$(this).data('selectedToAccount', true);
+			$(this).data('selectedFromAccount', false);
+
 			// store the id of this account
 			idOfSelectedToAccount = $(this).attr('id')
 
@@ -54,7 +70,13 @@ $(function(){
 
 			// add a border to the selected account to show it's selected
 			$('#' + idOfSelectedToAccount).css(selectedToAccountCss);
+
+			selectingToAccount = false;
 		} else if (selectingFromAccount) {
+			// mark this account as selected
+			$(this).data('selectedFromAccount', true);
+			$(this).data('selectedToAccount', false);
+
 			// store the id of this account
 			idOfSelectedFromAccount = $(this).attr('id')
 
@@ -66,6 +88,17 @@ $(function(){
 
 			// add a border to the selected account to show it's selected
 			$('#' + idOfSelectedFromAccount).css(selectedFromAccountCss);
+
+			selectingFromAccount = false;
+		} else if (selected) {
+			if (isToAccountSelected) {
+				$("#transferToAccount").text("");
+			} else if (isFromAccountSelected) {
+				$("#transferFromAccount").text("");
+			}
+
+			$(this).data(resetSelections)
+			$(this).css(emptyBorder);
 		}
 	});
 
