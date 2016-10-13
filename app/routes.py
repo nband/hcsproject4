4a2f6fd36fdfc59536ca7e1c8af26c5512ebdc08
Michael Ferraco <ferraco.mike@gmail.com>
2016-10-12 23:23:57 -0400

    Removed credit card logic

--- app/routes.py
+++ app/routes.py
@@ -41,17 +41,11 @@ def index():
 	if accountsResponse.status_code == 200:
 		accounts = json.loads(accountsResponse.text)
 
-		# filter out credit card accounts (can't transfer money to/from them)
-		accountsNoCards = []
-		for account in accounts:
-			if account["type"] != "Credit Card":
-				accountsNoCards.append(account);
-
 		# variable which will keep track of all transfers to pass to UI
 		transfers = []
 		
 		# for each account make a request to get it's transfers where it is the payer only...
-		for account in accountsNoCards:
+		for account in accounts:
 			transfersUrl = 'http://api.reimaginebanking.com/accounts/{}/transfers?type=payer&key={}'.format(account['_id'], apiKey)
 			transfersResponse = requests.get(transfersUrl)
 
@@ -59,7 +53,7 @@ def index():
 			if transfersResponse.status_code == 200:
 				transfers.extend(json.loads(transfersResponse.text))
 
-		return render_template("home.html", accounts=accountsNoCards, format_price=format_price, transfers=transfers)
+		return render_template("home.html", accounts=accounts, format_price=format_price, transfers=transfers)
 	else:
 		return render_template("notfound.html")
 
